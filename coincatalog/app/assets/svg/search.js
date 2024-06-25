08d911ea75374b97698888cafd1f28e55d77d732
LUZAmaxxp <ayman.allouch@e-polytechnique.ma>
2024-06-25 17:05:16 +0100

    feat : added a navbar

---
+++ coincatalog/app/assets/svg/search.js
@@ -0,0 +1,4 @@
+const Search = () => {
+  return <svg xmlns="https://www.w3s.org/2000/svg"></svg>;
+};
+export default Search;
--- coincatalog/app/page.tsx
+++ coincatalog/app/page.tsx
@@ -3,7 +3,10 @@ import Header from "./components/Header/Header";
 export default function Home() {
   return (
     <div className="min-h-screen">
-      <Header />
+      <div className="bg-black">
+        <Header />
+      </div>
+
       <div className="mt-10"></div>
       <div className="mt-20 "></div>
     </div>
