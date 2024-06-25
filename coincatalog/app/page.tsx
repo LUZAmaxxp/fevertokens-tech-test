c11c837d386929d9c962d0afe1222c91e8d2e655
LUZAmaxxp <ayman.allouch@e-polytechnique.ma>
2024-06-25 14:02:28 +0100

    feat : adding components

---
+++ coincatalog/app/page.tsx
@@ -0,0 +1,11 @@
+import Header from "./components/Header/Header";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen">
+      <Header />
+      <div className="mt-10"></div>
+      <div className="mt-20 "></div>
+    </div>
+  );
+}
--- coincatalog/src/app/pages/coins/[id].tsx
+++
@@ -1,9 +0,0 @@
-import { useRouter } from "next/router";
-import CoinDetail from "../../components/CoinDetail";
-
-export default function CoinDetailPage() {
-  const router = useRouter();
-  const { id } = router.query;
-
-  return <CoinDetail id={id} />;
-}
